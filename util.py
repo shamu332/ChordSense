f1787d04220b4fd8600aa2715c8e81869b1e469a
David Do <phamdavid102@gmail.com>
2023-03-11 19:29:21 -0500

    add graphing tools

---
+++ util.py
@@ -0,0 +1,13 @@
+import librosa as lr
+import matplotlib.pyplot as plt
+import pandas as pd
+
+def spectrogram(S):
+    fig, ax = plt.subplots(figsize=(15,5))
+    img = lr.display.specshow(S_db, x_axis="time", y_axis="log", ax=ax)
+    fig.colorbar(img, ax=ax, format=f'%0.2f')
+    plt.show()
+
+def raw(y):
+    pd.Series(y).plot(figsize=(10,5))
+    plt.show()
