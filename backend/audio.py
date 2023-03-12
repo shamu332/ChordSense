2d19f933dc8118cc8617e7036c9df257673e89eb
David Do <phamdavid102@gmail.com>
2023-03-12 11:19:19 -0400

    add svm classifier

---
+++ backend/audio.py
@@ -0,0 +1,56 @@
+import util
+import numpy as np
+import librosa as lr
+
+'''
+Returns array where row is a frame that include frequency intensities for each mel band
+'''
+def extract_frequency(file):
+    y, sr = lr.load(file)
+
+    y, _ = lr.effects.trim(y)
+
+    S = lr.feature.melspectrogram(y=y, sr=sr, n_mels=256)
+    S_db = lr.amplitude_to_db(S, ref=np.max)
+
+    S_db = S_db.T
+    S_db = lr.util.normalize(S_db, axis=1)
+
+    S_db_a = np.mean(S_db, axis=0)
+
+    ## DEBUG
+    # util.raw(y_trimmed)
+    # util.spec(S_db)
+    
+    return S_db_a
+
+def extract_chroma(file):
+    # Load audio file (or alternatively, you can use librosa to load audio from a file)
+    y, sr = lr.load(file)
+
+    y, _ = lr.effects.trim(y)
+
+    # Extract chroma features
+    chroma = lr.feature.chroma_stft(y=y, sr=sr)
+
+    # Transpose chroma matrix to match the format
+    chroma = chroma.T
+
+    # Normalize chroma features (optional)
+    # chroma = lr.util.normalize(chroma, axis=0)
+
+    # Print shape of chroma matrix (number of frames x number of chroma bands)
+    chroma_a = np.mean(chroma, axis=0)
+    return chroma_a
+
+'''
+Returns frequency bands of a mel-spectrogram
+Useful for headers
+'''
+def frequencies(S, sr):
+    return lr.core.mel_frequencies(n_mels=S.shape[0], fmin=0, fmax=sr/2)
+
+# S, sr = load("./piano_triads/A_dim_2_0.wav")
+
+# # Access the frequencies of n_mels
+# frequencies = frequencies(S, 256)
--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,12 @@ import librosa as lr
 from flask import send_file
 import os
 
+import audio
+import matplotlib.pyplot as plt
+
+from sklearn import svm
+from sklearn.metrics import accuracy_score, confusion_matrix, ConfusionMatrixDisplay
+from sklearn.model_selection import train_test_split
 
 
 app = Flask(__name__)
@@ -20,34 +26,72 @@ CORS(app)
 @app.route('/api/v1/audioAnalysis', methods=["POST"])
 def load():
     file = request.files['file']
-    y, sr = lr.load(file)
-
-    y_trimmed, _ = lr.effects.trim(y)
-
-    S = lr.feature.melspectrogram(y=y, sr=sr, n_mels=256)
-    S_db = lr.amplitude_to_db(S, ref=np.max)
-    S_db = lr.util.normalize(S_db, axis=1)
-    ## DEBUG
-    # util.raw(y_trimmed)
-    # util.spec(S_db)
     
-    S_db_a = np.mean(S_db.T, axis=0)
-    # return str(S_db_a), str(sr)
+    pred = routine(file)
+
     filename = "cute-beavers.png"  # Filename of the image file
     dir_path = os.path.dirname(os.path.realpath(__file__))  # Absolute path of the app directory
     file_path = os.path.join(dir_path, filename)  # Absolute path of the image file
-    return send_file(file_path, mimetype='image/png')
+    return pred
+
+def load_model():
+    if (os.path.exists('pianoX.npy') and os.path.exists('pianoY.npy')):
+        print("Loaded from cache!")
+        X = np.load("pianoX.npy")
+        Y = np.load("pianoY.npy")
+        return X, Y
+    
+    print("Reading audio files!")
+    X = []
+    Y = []
+    # group files by chord
+    for f in os.listdir('./piano_triads'):
+        path = os.path.join('./piano_triads', f)
+
+        chord, _ = os.path.splitext(f)
+        chord = chord.split('_')
+        chord = chord[0] + '-' + chord[1]
+
+        X.append(audio.extract_chroma(path))
+        Y.append(chord)
+
+    np.save('pianoX.npy', X)
+    np.save('pianoY.npy', Y)
+    return X, Y
+
+def routine(file):
+    ## ML Begins
+    X, Y = load_data()
+    X_train, X_valid, Y_train, Y_valid = train_test_split(X, Y, test_size=0.80, random_state=0)
+
+    # Train a SVM
+    clf = svm.SVC(kernel='linear', C=8)
+    clf.fit(X_train, Y_train)
+
+    X_valid = audio.extract_chroma(file)
+
+    # Evaluate the SVM on the test data
+    Y_pred = clf.predict(X_valid)
+
+    return Y_pred
 
 
+def demo_routine():
+    ## ML Begins
+    X, Y = load_data()
+    X_train, X_valid, Y_train, Y_valid = train_test_split(X, Y, test_size=0.33, random_state=0)
 
-'''
-Returns frequency bands of a mel-spectrogram
-Useful for headers
-'''
-def frequencies(S, sr):
-    return lr.core.mel_frequencies(n_mels=S.shape[0], fmin=0, fmax=sr/2)
+    # Train a SVM
+    clf = svm.SVC(kernel='linear', C=8)
+    clf.fit(X_train, Y_train)
 
-# S, sr = load("./piano_triads/A_dim_2_0.wav")
+    # Evaluate the SVM on the test data
+    Y_pred = clf.predict(X_valid)
 
-# # Access the frequencies of n_mels
-# frequencies = frequencies(S, sr)
+    return Y_pred
+
+## STATS
+# print(accuracy_score(Y_valid, Y_pred))
+# fig, ax = plt.subplots(figsize=(20, 35))
+# cm = ConfusionMatrixDisplay.from_predictions(Y_valid, Y_pred, display_labels=clf.classes_, ax=ax, colorbar=False)
+# plt.savefig('cmatrix.png')
