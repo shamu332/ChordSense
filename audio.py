8a8c786f30f213f3becec2311317337a71cb0263
David Do <phamdavid102@gmail.com>
2023-03-11 20:19:57 -0500

    add chroma extraction

--- audio.py
+++ audio.py
@@ -5,20 +5,43 @@ import librosa as lr
 '''
 Returns array where row is a frame that include frequency intensities for each mel band
 '''
-def load(file):
+def extract_frequency(file):
     y, sr = lr.load(file)
 
-    y_trimmed, _ = lr.effects.trim(y)
+    y, _ = lr.effects.trim(y)
 
     S = lr.feature.melspectrogram(y=y, sr=sr, n_mels=256)
     S_db = lr.amplitude_to_db(S, ref=np.max)
+
+    S_db = S_db.T
     S_db = lr.util.normalize(S_db, axis=1)
+
+    S_db_a = np.mean(S_db, axis=0)
+
     ## DEBUG
     # util.raw(y_trimmed)
     # util.spec(S_db)
     
-    S_db_a = np.mean(S_db.T, axis=0)
     return S_db_a, sr
+
+def extract_chroma(file):
+    # Load audio file (or alternatively, you can use librosa to load audio from a file)
+    y, sr = lr.load(file)
+
+    y, _ = lr.effects.trim(y)
+
+    # Extract chroma features
+    chroma = lr.feature.chroma_stft(y=y, sr=sr)
+
+    # Transpose chroma matrix to match the format used by scikit-learn and other libraries
+    chroma = chroma.T
+
+    # Normalize chroma features (optional)
+    chroma = lr.util.normalize(chroma, axis=0)
+
+    # Print shape of chroma matrix (number of frames x number of chroma bands)
+    chroma_a = np.mean(chroma, axis=0)
+    return chroma_a
 
 '''
 Returns frequency bands of a mel-spectrogram
