5691af5fbb298c0ef769738b99870dcb2eff9d32
David Do <phamdavid102@gmail.com>
2023-03-11 20:20:43 -0500

    bad parameter name

--- util.py
+++ util.py
@@ -2,7 +2,7 @@ import librosa as lr
 import matplotlib.pyplot as plt
 import pandas as pd
 
-def spectrogram(S):
+def spectrogram(S_db):
     fig, ax = plt.subplots(figsize=(15,5))
     img = lr.display.specshow(S_db, x_axis="time", y_axis="log", ax=ax)
     fig.colorbar(img, ax=ax, format=f'%0.2f')
